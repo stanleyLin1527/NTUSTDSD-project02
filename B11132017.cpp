75ae72d20dcd20d863cedb807e8c353bcb3154af
stanleyLin0402 <lin03.kw@gmail.com>
2023-09-25 23:28:21 +0800

    Modify the wat of reading file & build blueprint

--- B11132017.cpp
+++ B11132017.cpp
@@ -7,38 +7,64 @@
 #include "Product.h"
 using namespace std;
 
-#define char_zero '0'
 #define not_care '-'
 
-void read_PLA_file(ifstream &in_PLA_file, int &var_amount, int &pro_amount, vector<string> &var_labels, vector<Product> &products);
+void read_PLA_file(ifstream &in_PLA_file, int &var_amount, int &pro_amount, string &out_label,
+                   vector<string> &var_labels, vector<Product> &products);
 
-void write_PLA_file(ofstream& out_PLA_file, vector<string> &var_labels, vector<Product> &new_products);
+vector<Product> make_minterm_list(int &var_amount);
+
+vector<Product> products_to_minterms(vector<Product> &products, vector<Product> &minterms_list);
+
+vector<Product> QA_algorithm(vector<Product> &minterms);
+
+void Patrick_method(vector<Product> &PI);
+
+void write_PLA_file(ofstream &out_PLA_file, string &out_label, vector<string> &var_labels, vector<Product> &EPI);
 
 int main()
 {
     int var_amount;
     int pro_amount = 0;
+    string out_label;
     vector<string> var_labels;
     vector<Product> products;
     ifstream in_PLA_file("Pro_1.pla");
     ofstream out_PLA_file("Pro_1_out.pla");
 
-    read_PLA_file(in_PLA_file, var_amount, pro_amount, var_labels, products);
+    read_PLA_file(in_PLA_file, var_amount, pro_amount, out_label, var_labels, products);
     in_PLA_file.close();
 
     // Unit test to check the pla. file is read correctly
-    // cout << var_amount << " " << pro_amount << endl;
-    // for (string label : var_labels)
-    //     cout << label << " ";
-    // cout << endl;
-    // for (Product x : products)
-    //     cout << x << endl;
-    // return 0;
+    cout << var_amount << " " << pro_amount << endl;
+    for (string label : var_labels)
+        cout << label << " ";
+    cout << endl;
+    cout << out_label << endl;
+    for (Product x : products)
+        cout << x.literals << " " << x.type << endl;
+    return 0;
+
+    vector<Product> list = make_minterm_list(var_amount);
+    vector<Product> minterms = products_to_minterms(products, list);
+    // TODO: Unit test to check all products are convert into minterms correctly
+
+    vector<Product> PI = QA_algorithm(minterms);
+    // TODO: Unit test to check all PIs are correct
+
+    vector<Product> EPI;
+    // TODO: Find minterms which are wrapped by only one PI and push them into EPI first, and then remove them from PI
+
+    Patrick_method(PI); // Find required EPI from the remaining PIs
+
+    write_PLA_file(out_PLA_file, out_label, var_labels, EPI);
+    out_PLA_file.close();
 
     return 0;
 }
 
-void read_PLA_file(ifstream &in_PLA_file, int &var_amount, int &pro_amount, vector<string> &var_labels, vector<Product> &products)
+void read_PLA_file(ifstream &in_PLA_file, int &var_amount, int &pro_amount, string &out_label,
+                   vector<string> &var_labels, vector<Product> &products)
 {
     string input;
     stringstream line;
@@ -68,48 +94,25 @@ void read_PLA_file(ifstream &in_PLA_file, int &var_amount, int &pro_amount, vect
             {
                 line.clear(); // To clear string stream
                 line.str(""); // To clear string stream
-                string product;
-                Product tmp; // A product which would be push into vector later
-                char type;
+                Product tmp;  // A product which would be push into vector later
                 getline(in_PLA_file, input);
                 line << input;
-                line >> product;
-
-                for (int i = 0; i < var_amount; i++)
-                {
-                    if (product[i] == not_care)
-                        tmp.literals.push_back(-1);
-                    else
-                        tmp.literals.push_back(product[i] - char_zero);
-                }
-                line >> type;
-                if (type == not_care)
-                    tmp.type = -1;
-                else
-                    tmp.type = type - char_zero;
+                line >> tmp.literals;    // Fill literals
+                line >> tmp.type;        // Fill type
                 products.push_back(tmp); // Push product
             }
         }
+        else if (command == ".ob")
+            line >> out_label;
         else
         {
             // If there's no .p command, then when the first char is not '.',
             // that line is the beginning of products and start counting
             if (command[0] != '.')
             {
-                Product tmp; // A product which would be push into vector later
-                char type;
-                for (int i = 0; i < var_amount; i++)
-                {
-                    if (command[i] == not_care)
-                        tmp.literals.push_back(-1);
-                    else
-                        tmp.literals.push_back(command[i] - char_zero);
-                }
-                line >> type;
-                if (type == not_care)
-                    tmp.type = -1;
-                else
-                    tmp.type = type - char_zero;
+                Product tmp;             // A product which would be push into vector later
+                tmp.literals = command;  // Fill literals
+                line >> tmp.type;        // Fill type
                 products.push_back(tmp); // Push product
                 pro_amount++;
             }
@@ -117,4 +120,39 @@ void read_PLA_file(ifstream &in_PLA_file, int &var_amount, int &pro_amount, vect
                 continue; // Discard useless line of operation
         }
     }
+}
+
+// TODO: Complete it
+vector<Product> make_minterm_list(int &var_amount)
+{
+    vector<Product> list;
+
+    return list;
+}
+
+// TODO: Complete it
+vector<Product> products_to_minterms(vector<Product> &products, vector<Product> &minterms_list)
+{
+    vector<Product> minterms;
+
+    return minterms;
+}
+
+// TODO: Complete it
+vector<Product> QA_algorithm(vector<Product> &minterms)
+{
+    vector<Product> PI;
+
+    return PI;
+}
+
+// TODO: Complete it
+void Patrick_method(vector<Product> &PI)
+{
+}
+
+// TODO: Complete it
+void write_PLA_file(ofstream &out_PLA_file, string &out_label, vector<string> &var_labels, vector<Product> &EPI)
+{
+    cout << ".e";
 }
--- Product.h
+++ Product.h
@@ -1,36 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
 using namespace std;
 
 class Product
 {
 public:
-    vector<int> literals;
-    int type;
-
-    friend ostream &operator<<(ostream &out, Product &product)
-    {
-        for (int x : product.literals){
-            if (x == -1)
-                out << "-";
-            else
-                out << x;
-        }
-        out << " " << product.type;
-        return out;
-    }
-
-    friend ofstream &operator<<(ofstream &out, Product &product)
-    {
-        for (int x : product.literals)
-        {
-            if (x == -1)
-                out << "-";
-            else
-                out << x;
-            out << " " << product.type;
-        }
-        return out;
-    }
+    string literals;
+    vector<int> added_minterms;
+    char type;
+    bool has_checked = false;
 };
